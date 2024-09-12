bbfba84ccb16e635700bc89e553f83255373908b
satvikkumar11 <111700708+satvikkumar11@users.noreply.github.com>
2024-09-12 06:06:04 +0000

    Map initial

--- hydroguard/src/Pages/Home.jsx
+++ hydroguard/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Footer from '../Component/Footer/Footer'
 import Slide from '../Component/Slide/Slide'
 import Middle from '../Component/Middle/Middle'
+import Map from '../Component/Map/Map'
+
 
 
 export const Home = () => {
@@ -11,7 +13,9 @@ export const Home = () => {
       
         <Slide/>
         <Middle/>
+        <Map/>
         <Footer/>
+        
     </div>
   )
 }
