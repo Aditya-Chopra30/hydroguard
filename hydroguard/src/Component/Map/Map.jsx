9cdae128f6cc4b354b9c1e6d9a80a539028e7ad3
satvikkumar11 <111700708+satvikkumar11@users.noreply.github.com>
2024-09-12 06:29:51 +0000

    MapModify

--- hydroguard/src/Component/Map/Map.jsx
+++ hydroguard/src/Component/Map/Map.jsx
@@ -1,28 +1,69 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
-import "./Map.css";
+// import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+// import { useMemo } from "react";
+// import "./Map.css";
 
-const Map = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.AIzaSyAxBSEI1N8K62udtj7NR794xI-JENaypKU,
+// const Map = () => {
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: process.env.AIzaSyAxBSEI1N8K62udtj7NR794xI-JENaypKU,
+//   });
+//   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+//   return (
+//     <div className="App">
+//       {!isLoaded ? (
+//         <h1>Loading...</h1>
+//       ) : (
+//         <GoogleMap
+//           mapContainerClassName="map-container"
+//           center={center}
+//           zoom={10}
+//         >
+//           <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+//         </GoogleMap>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Map;
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat:  26.4499, // default latitude
+  lng: 80.3319, // default longitude
+};
+
+const App = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAxBSEI1N8K62udtj7NR794xI-JENaypKU',
+    libraries,
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
 
   return (
-    <div className="App">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          zoom={10}
-        >
-          <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
-        </GoogleMap>
-      )}
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
   );
 };
 
-export default Map;
+export default App;
